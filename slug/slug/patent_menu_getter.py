22e66d75d41272d36c10f11340de53b8c70fe7b3
errorzhu <982004587@qq.com>
2021-01-05 21:40:26 +0800

    test request entry

--- slug/slug/patent_menu_getter.py
+++ slug/slug/patent_menu_getter.py
@@ -7,11 +7,11 @@ current_dir = os.path.dirname(__file__)
 
 class PatentMenuGetter():
     def __init__(self):
-        self.base_url = "https://worldwide.espacenet.com/3.2/rest-services/search"
+        self.base_url = "https://worldwide.espacenet.com/3.2/rest-services/search?lang=en%2Cde%2Cfr&q=A61K38%2F00&qlang=cql&p_s=espacenet&p_q=A61K38%2F00"
         self.headers = {
             'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
             'Content-Type': 'application/json',
-            'EPO-Trace-Id': 's347yb-89q317-AAA-000000',
+            #'EPO-Trace-Id': 's347yb-89q317-AAA-000000',
             'X-EPO-PQL-Profile': 'cpci',
             'Origin': 'https://worldwide.espacenet.com',
             'Sec-Fetch-Site': 'same-origin',
@@ -21,8 +21,10 @@ class PatentMenuGetter():
             }
 
     def request(self, param, data):
-        url = self.base_url + "?" + "q=" + param
-        response = requests.post(url, headers=self.headers, data=json.dumps(data), verify=False)
+        session = requests.session()
+        url = self.base_url
+        session.head("https://worldwide.espacenet.com/patent/?EPOTraceID=igd8d7-u7u4s5&event=onSearchSubmit_adv-off&lgCC=en_EP&listView=text-only")
+        response = session.post(url, headers=self.headers, data=json.dumps(data), verify=False)
         print(response.text)
 
 
