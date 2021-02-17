3492fbc71f66b64de65204e9b5471db84e87da1d
errorzhu <982004587@qq.com>
2021-02-17 10:41:22 +0800

    test new method

--- slug/slug/pdf2txt_test.py
+++ slug/slug/pdf2txt_test.py
@@ -4,6 +4,12 @@ from pdfminer.layout import LAParams
 from io import StringIO
 from io import open
 import os
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal, LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+
 
 def read_pdf(pdfFile):
     rsrcmgr = PDFResourceManager()
@@ -19,11 +25,62 @@ def read_pdf(pdfFile):
     return content
 
 
+def parse(pdf_path, txt_path):
+    # 解析PDF文本，并保存到TXT文件中
+    fp = open(pdf_path, 'rb')
+    # 用文件对象创建一个PDF文档分析器
+    parser = PDFParser(fp)
+    # 创建一个PDF文档
+    doc = PDFDocument()
+    # 连接分析器，与文档对象
+    parser.set_document(doc)
+    doc.set_parser(parser)
+
+    # 提供初始化密码，如果没有密码，就创建一个空的字符串
+    doc.initialize()
+
+    # 检测文档是否提供txt转换，不提供就忽略
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    else:
+        # 创建PDF，资源管理器，来共享资源
+        rsrcmgr = PDFResourceManager()
+        # 创建一个PDF设备对象
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        # 创建一个PDF解释器对象
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # 循环遍历列表，每次处理一个page内容
+        # doc.get_pages() 获取page列表
+        for page in doc.get_pages():
+            interpreter.process_page(page)
+            # 接受该页面的LTPage对象
+            layout = device.get_result()
+            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
+            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
+            # 想要获取文本就获得对象的text属性，
+            for x in layout:
+                print("hello")
+
+                with open(txt_path, 'a+', encoding='utf-8') as f:
+                    results = x.get_text()
+                    print(results)
+                    f.write(results + "\n")
+                #if (isinstance(x, LTTextBoxHorizontal)):
+
+
+
 if __name__ == '__main__':
     current_dir = os.path.dirname(__file__)
     patent_dir = os.path.join(current_dir, "patent")
-    pdf=os.path.join(patent_dir,"AT285813B.pdf")
+    pdf=os.path.join(patent_dir,"DE1038239B.pdf")
     print(pdf)
-    content = read_pdf(open(pdf, 'rb'))
-    with open("AT285813B.txt","a",encoding="utf8") as ff:
-        ff.write(content)
+    #content = read_pdf(open(pdf, 'rb'))
+    pdf_path = pdf
+    txt_path = 'DE1038239B.txt'
+    parse(pdf_path, txt_path)
+
+
+    # with open("DE1038239B.txt","a",encoding="utf8") as ff:
+    #     ff.write(content)
